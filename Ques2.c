4b91c3ffe3a9838ba8151645e21608d3306a8d64
Harshit-CU12 <mrharshityadav179@gmail.com>
2026-08-09 08:00:19 +0530

    Add initial implementations for area calculations of square and circle

---
+++ Ques2.c
@@ -0,0 +1,10 @@
+// Write a program to calculate area of circle. (Radius is given)
+#include <stdio.h>
+int main(){
+    float radius;
+    printf("Enter Radius");
+    scanf("%f",&radius);
+
+    printf("area is ; %f", 3.14 * radius * radius);
+    return 0;
+}
