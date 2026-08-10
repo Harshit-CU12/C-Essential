82c0c453ca43147835633c74d27b7104bf7a9e0b
Harshit-CU12 <mrharshityadav179@gmail.com>
2026-08-10 13:25:14 +0530

    chore: clean up VS Code C/C++ runner settings

--- Ques2.c
+++ Ques2.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 int main(){
     float radius;
-    printf("Enter Radius");
+    printf("Enter Radius: ");
     scanf("%f",&radius);
 
-    printf("area is ; %f", 3.14 * radius * radius);
+    printf("area is: %f", 3.14 * radius * radius);
     return 0;
 }
