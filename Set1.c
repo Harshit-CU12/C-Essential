6dfb4057cc70c214511ee9a42f610ae4a5c0accb
Harshit-CU12 <mrharshityadav179@gmail.com>
2026-08-11 22:01:13 +0530

    Refactor code structure for improved readability and maintainability

---
+++ Set1.c
@@ -0,0 +1,10 @@
+//Set of some questions of c language
+#include <stdio.h>
+int main(){
+    int a=10;
+    int b=20;
+    printf("Modulo of  a and b is :%d\n",a%b);
+    printf("Division of a and b is: %d\n",a/b);
+    printf("Multiplication of a and b is: %d\n",a*b);
+    return 0;
+}
