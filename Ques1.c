42da3dc85d895eb63107d03266656b616cc2f021
Harshit-CU12 <mrharshityadav179@gmail.com>
2026-09-17 11:32:20 +0530

    Update binary files Ques1.exe and Ques2.exe

--- Ques1.c
+++ Ques1.c
@@ -1,10 +1,12 @@
 //Write a program to calculate area of a Square.(Side is given)
 #include <stdio.h>
 int main(){
+    /*
     int side;
     printf("Enter side: ");
     scanf("%d", &side);
 
-    printf("Area is: %d", side * side);
+    printf("Area is: %d", side * side);*/
+    
     return 0;
 }
