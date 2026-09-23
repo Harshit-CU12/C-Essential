a3fd8041b941acd75f1da624096f3d3e1d9068ba
Harshit-CU12 <mrharshityadav179@gmail.com>
2026-09-23 10:48:58 +0530

    Add initial implementation of array input and output functionality

---
+++ Arr.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5];
+    for(int i=0;i<5;i++)
+    {
+        printf("Enter element %d:",i+1);
+        scanf("%d",&arr[1]);
+    }
+
+    printf("Array element are:");
+    for(int i=0;i<5;i++){
+        printf("%d",arr[i]);
+    }
+    return 0;
+}
