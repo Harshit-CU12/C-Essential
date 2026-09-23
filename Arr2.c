557e0a63955cd387d08e7e3530710112e9aefdc3
Harshit-CU12 <mrharshityadav179@gmail.com>
2026-09-23 10:53:43 +0530

    Refactor code structure for improved readability and maintainability

--- Arr2.c
+++ Arr2.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
 
-int main() {
-    int arr[5];
-    for(int i=0;i<5;i++)
-    {
-        printf("Enter element %d:",i+1);
-        scanf("%d",&arr[1]);
-    }
-
-    printf("Array element are:");
-    for(int i=0;i<5;i++){
-        printf("%d",arr[i]);
+int main(void) {
+int marks[5]={85,86,87,88,89};
+    for( int i=0;i<5;i++){
+    printf("%d\n",marks[i]);
     }
     return 0;
 }
